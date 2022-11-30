aba3588857ea6aadd6f61019897dc1531d6c1904
andrechizh8 <andrechizh.ru@yandex.ru>
2022-11-30 21:26:53 +0300

    third version

--- 1.py
+++ 1.py
@@ -1,9 +1,9 @@
 def open_browser(browser_name):
-    my_func_1(open_browser, browser_name)
+    change_name_func(open_browser, browser_name)
 
 
-def my_func_1(x, y):
-    print(x.__name__.upper().replace('_', ' '), y.upper())
+def change_name_func(name, arg):
+    print(name.__name__.upper().replace('_', ' '),arg.upper())
 
 
 open_browser('Chrome')
@@ -13,8 +13,8 @@ def go_to_companyname_homepage(page_url):
     my_func_2(go_to_companyname_homepage, page_url)
 
 
-def my_func_2(x, y):
-    print(x.__name__.capitalize().replace('_', ' '), y[8:-1])
+def my_func_2(name, url):
+    print(name.__name__.capitalize().replace('_', ' '), url[8:-1])
 
 
 go_to_companyname_homepage('https://yandex.ru/')
@@ -24,8 +24,8 @@ def find_registration_button_on_login_page(page_url, button_text):
     my_func_3(find_registration_button_on_login_page, page_url, button_text)
 
 
-def my_func_3(x, y, z):
-    print(x.__name__.capitalize().replace('_', ' '), y, z)
+def my_func_3(name, url, text):
+    print(name.__name__.capitalize().replace('_', ' '), url, text)
 
 
 find_registration_button_on_login_page('https://google.com/', 'submit')
